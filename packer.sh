a7a80d759bd8b2e7d4fc2e300254b80fcdd7fcfa
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-09 07:13:06 +1000

    Need to init

--- packer.sh
+++ packer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 chmod +x packer/packer
-packer/packer validate ubuntu-webserver.hcl
+packer/packer init ubuntu-webserver.pkr.hcl
+packer/packer validate ubuntu-webserver.pkr.hcl
 packer/packer build "-var=vpc=#{AWS.Network.VPCID}" "-var=subnet=#{AWS.Network.SubnetID}" ubuntu-webserver.pkr.hcl
