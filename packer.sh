da689caee3de4f92e4dfddb33bc52f2b62301a9d
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-09 08:23:43 +1000

    Disable color

--- packer.sh
+++ packer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod +x packer/packer
-packer/packer init ubuntu-webserver.pkr.hcl
-packer/packer validate "-var=vpc=#{AWS.Network.VPCID}" "-var=subnet=#{AWS.Network.SubnetID}" ubuntu-webserver.pkr.hcl
-packer/packer build "-var=vpc=#{AWS.Network.VPCID}" "-var=subnet=#{AWS.Network.SubnetID}" ubuntu-webserver.pkr.hcl
+packer/packer init -color=false ubuntu-webserver.pkr.hcl
+packer/packer validate -color=false "-var=vpc=#{AWS.Network.VPCID}" "-var=subnet=#{AWS.Network.SubnetID}" ubuntu-webserver.pkr.hcl
+packer/packer build -color=false "-var=vpc=#{AWS.Network.VPCID}" "-var=subnet=#{AWS.Network.SubnetID}" ubuntu-webserver.pkr.hcl
