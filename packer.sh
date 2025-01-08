47c9ddb2c0bed34eb03cbdf827e2e468075a326b
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-09 07:11:56 +1000

    Fixed filenames

--- packer.sh
+++ packer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x packer/packer
 packer/packer validate ubuntu-webserver.hcl
-packer/packer build "-var=vpc=#{AWS.Network.VPCID}" "-var=subnet=#{AWS.Network.SubnetID}" ubuntu-webserver.hcl
+packer/packer build "-var=vpc=#{AWS.Network.VPCID}" "-var=subnet=#{AWS.Network.SubnetID}" ubuntu-webserver.pkr.hcl
