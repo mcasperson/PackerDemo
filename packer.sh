22f39cd6942d6a2856cc797de1b8ae71ad4e6beb
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-09 07:10:42 +1000

    Fixed path

--- packer.sh
+++ packer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd packer
-chmod +x packer
-./packer validate ubuntu-webserver.hcl
-./packer build "-var=vpc=#{AWS.Network.VPCID}" "-var=subnet=#{AWS.Network.SubnetID}" ubuntu-webserver.hcl
+chmod +x packer/packer
+packer/packer validate ubuntu-webserver.hcl
+packer/packer build "-var=vpc=#{AWS.Network.VPCID}" "-var=subnet=#{AWS.Network.SubnetID}" ubuntu-webserver.hcl
