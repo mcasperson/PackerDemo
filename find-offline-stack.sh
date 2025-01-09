8adf165d2249336affb6ad62dd0ded3e084c6cc1
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-09 10:36:10 +1000

    Extracting weights

--- find-offline-stack.sh
+++ find-offline-stack.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-aws elbv2 describe-rules \
+RULES=$(aws elbv2 describe-rules \
   --listener-arn "#{AWS.ALB.Listener}" \
-  --output json
+  --output json)
+
+echo "${RULES}"
+
+GREENWEIGHT=$(jq -r '.Rules[] | select(.RuleArn == "#{AWS.ALB.Rule}") | .Actions[] | select(.Type == "forward") | .ForwardConfig | .TargetGroups[] | select(.TargetGroupArn == "#{AWS.ALB.GreenTargetGroup}" | .Weight)' <<< "${RULES}")
+BLUEWEIGHT=$(jq -r '.Rules[] | select(.RuleArn == "#{AWS.ALB.Rule}") | .Actions[] | select(.Type == "forward") | .ForwardConfig | .TargetGroups[] | select(.TargetGroupArn == "#{AWS.ALB.BlueTargetGroup}" | .Weight)' <<< "${RULES}")
+
+echo "Green weight: ${GREENWEIGHT}"
+echo "Blue weight: ${BLUEWEIGHT}"
