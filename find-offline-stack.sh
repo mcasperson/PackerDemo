a53170bf48feb964575a26292041f32931e747a2
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-09 10:38:40 +1000

    Save output as artifact

--- find-offline-stack.sh
+++ find-offline-stack.sh
@@ -5,6 +5,9 @@ RULES=$(aws elbv2 describe-rules \
   --output json)
 
 echo "${RULES}"
+echo "${RULES}" > output.json
+
+new_octopusartifact output.json
 
 GREENWEIGHT=$(jq -r '.Rules[] | select(.RuleArn == "#{AWS.ALB.Rule}") | .Actions[] | select(.Type == "forward") | .ForwardConfig | .TargetGroups[] | select(.TargetGroupArn == "#{AWS.ALB.GreenTargetGroup}" | .Weight)' <<< "${RULES}")
 BLUEWEIGHT=$(jq -r '.Rules[] | select(.RuleArn == "#{AWS.ALB.Rule}") | .Actions[] | select(.Type == "forward") | .ForwardConfig | .TargetGroups[] | select(.TargetGroupArn == "#{AWS.ALB.BlueTargetGroup}" | .Weight)' <<< "${RULES}")
