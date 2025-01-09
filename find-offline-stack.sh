f86e37e8815355212d605951ff8443b0bf599d9c
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-09 10:49:08 +1000

    Cleaned up output

--- find-offline-stack.sh
+++ find-offline-stack.sh
@@ -4,10 +4,9 @@ RULES=$(aws elbv2 describe-rules \
   --listener-arn "#{AWS.ALB.Listener}" \
   --output json)
 
-echo "${RULES}"
-echo "${RULES}" > output.json
-
-new_octopusartifact "$PWD/output.json"
+#echo "${RULES}"
+#echo "${RULES}" > output.json
+#new_octopusartifact "$PWD/output.json"
 
 GREENWEIGHT=$(jq -r '.Rules[] | select(.RuleArn == "#{AWS.ALB.ListenerRule}") | .Actions[] | select(.Type == "forward") | .ForwardConfig | .TargetGroups[] | select(.TargetGroupArn == "#{AWS.ALB.GreenTargetGroup}") | .Weight' <<< "${RULES}")
 BLUEWEIGHT=$(jq -r '.Rules[] | select(.RuleArn == "#{AWS.ALB.ListenerRule}") | .Actions[] | select(.Type == "forward") | .ForwardConfig | .TargetGroups[] | select(.TargetGroupArn == "#{AWS.ALB.BlueTargetGroup}") | .Weight' <<< "${RULES}")
@@ -21,5 +20,5 @@ if [ "${GREENWEIGHT}" == "100" ]; then
   set_octopusvariable "InactiveGroupArn" "#{AWS.ALB.BlueTargetGroup}"
 else
   set_octopusvariable "ActiveGroupArn" "#{AWS.ALB.BlueTargetGroup}"
-    set_octopusvariable "InactiveGroupArn" "#{AWS.ALB.GreenTargetGroup}"
+  set_octopusvariable "InactiveGroupArn" "#{AWS.ALB.GreenTargetGroup}"
 fi
