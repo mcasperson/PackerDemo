c257a1dfbd53f65f7ddcfb81b93ca552f9c9499d
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-09 13:14:31 +1000

    Fixing script

--- modify-lb-listener-rule.sh
+++ modify-lb-listener-rule.sh
@@ -2,7 +2,7 @@
 
 MODIFYRULE=$(aws elbv2 modify-rule \
   --rule-arn "#{AWS.ALB.ListenerRule}" \
-  --actions "Type=forward,TargetGroupArn=#{Octopus.Action[Find Offline Target Group].Output.ActiveGroupArn},Weight=0" "Type=forward,TargetGroupArn=#{Octopus.Action[Find Offline Target Group].Output.InactiveGroupArn},Weight=100")
+  --actions "Type=forward,TargetGroupArn=#{Octopus.Action[Find Offline Target Group].Output.ActiveGroupArn},ForwardConfig={TargetGroups={Weight=0}}" "Type=forward,TargetGroupArn=#{Octopus.Action[Find Offline Target Group].Output.InactiveGroupArn},ForwardConfig={TargetGroups={Weight=100}}")
 
 echo "Updated listener rules for #{AWS.ALB.ListenerRule} to set weight to 0 for #{Octopus.Action[Find Offline Target Group].Output.ActiveGroupArn} and 100 for #{Octopus.Action[Find Offline Target Group].Output.InactiveGroupArn}"
 
