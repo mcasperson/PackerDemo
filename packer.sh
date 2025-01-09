af10885c5c36f48b6cdfa988e19e6349a53ea430
Matthew Casperson <matthewcasperson@gmail.com>
2025-01-09 14:42:49 +1000

    Fixed subnets

--- packer.sh
+++ packer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod +x packer/packer
 packer/packer init ubuntu-webserver.pkr.hcl
-packer/packer validate "-var=vpc=#{AWS.Parameter.VPCID}" "-var=subnet=#{AWS.Parameter.SubnetID}" ubuntu-webserver.pkr.hcl
-packer/packer build -color=false "-var=vpc=#{AWS.Parameter.VPCID}" "-var=subnet=#{AWS.Parameter.SubnetID}" ubuntu-webserver.pkr.hcl
+packer/packer validate "-var=vpc=#{AWS.Parameter.VPCID}" "-var=subnet=#{AWS.Parameter.SubnetIdAz1}" ubuntu-webserver.pkr.hcl
+packer/packer build -color=false "-var=vpc=#{AWS.Parameter.VPCID}" "-var=subnet=#{AWS.Parameter.SubnetIdAz1}" ubuntu-webserver.pkr.hcl
